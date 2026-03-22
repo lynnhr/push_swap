f92df6ba3d3d39a86ba71af845e923a5b993417f
lynn <lynn@example.com>
2026-03-22 02:02:57 +0200

    Attempting Radix Sort

--- source/sorting/ft_radix_sort.c
+++ source/sorting/ft_radix_sort.c
@@ -6,13 +6,39 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 03:03:59 by marvin            #+#    #+#             */
-/*   Updated: 2026/03/22 00:56:55 by marvin           ###   ########.fr       */
+/*   Updated: 2026/03/22 01:55:14 by marvin           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "ft_push_swap.h"
 
-// int	ft_count_bits(int n)
-// {
+int	ft_count_bits(int n)
+{
+    int	bits;
+	
+	bits = 0;
+	while (n >> bits)
+		bits++;
+	return (bits);
+}
 
-// }
+void	ft_radix_sort(t_stack *a, t_stack *b)
+{
+	int	bits;
+	int	i;
+	t_node	*current;
+	
+	current = a -> head;
+	i = 0;
+	bits = ft_count_bits(a->value);
+	while( i <= bits)
+	{
+		while(current)
+		{
+			if()
+			current = current->next;
+		}
+		current-> value >> 1;
+		i++;
+	}
+}
